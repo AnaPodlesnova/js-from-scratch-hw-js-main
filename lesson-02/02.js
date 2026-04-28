b5c84f2f9842df57a8f7f818fb2657af22a3ee86
AnaPodlesnova <anet258704@gmail.com>
2026-04-28 18:41:47 +0300

    v-1

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,11 +7,14 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
+let dice1 =  Math.round (Math.random() * 5)// your code
+let dice2 =  Math.round (Math.random() * 5)// your code
 let isWinningDouble // your code
 
-/*
+if(dice1 === dice2){
+  isWinningDouble = true
+}
+
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
 if (isWinningDouble) {
@@ -19,4 +22,4 @@ if (isWinningDouble) {
 } else {
   console.log('Не выигрышный дубль.')
 }
-*/
+
