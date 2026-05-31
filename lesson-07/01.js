404f5909e285847cad099ad52808db07d43ac89e
AnaPodlesnova <anet258704@gmail.com>
2026-05-31 15:56:30 +0300

    v-1

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -17,3 +17,27 @@ const WEB_TECH_IMAGES = [
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
 ]
+
+const slider = document.getElementById("web-tech-image")
+const prevButton = document.getElementById('prev-button')
+const nextButton = document.getElementById('next-button')
+
+let index = 0;
+
+prevButton.addEventListener("click", () => {
+  index --;
+
+  if (index < 0) {
+    index = WEB_TECH_IMAGES.length -1;
+  }
+  slider.src = WEB_TECH_IMAGES[index]
+})
+
+nextButton.addEventListener("click", () => {
+  index++;
+
+  if (index > WEB_TECH_IMAGES.length ) {
+    index = 0
+  }
+  slider.src = WEB_TECH_IMAGES[index]
+})
